d8e34b3b861fa946d9d2c7216627cb91cc1323fd
Sebastian Irsch <com@seb.in2u.eu>
2023-01-22 14:56:32 +0100

    Klasse LogOutputManager bereitgestellt

---
+++ src/main/java/software/sirsch/sa4e/puzzles/LogOutputManager.java
@@ -0,0 +1,227 @@
+package software.sirsch.sa4e.puzzles;
+
+import java.io.BufferedReader;
+import java.io.Console;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Optional;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+import javax.annotation.CheckForNull;
+import javax.annotation.Nonnull;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * Diese Klasse stellt die Schaltstelle zum Umschalten der Log-Ausgabe bereit.
+ *
+ * @author sirsch
+ * @since 21.01.2023
+ */
+public class LogOutputManager {
+
+	/**
+	 * Dieses Feld soll die Singleton-Instance dieser Klasse enthalten.
+	 */
+	@Nonnull
+	private static final LogOutputManager SINGLETON_INSTANCE = new LogOutputManager(
+			PuzzleSolverFactory.getSingletonInstance(),
+			Optional.ofNullable(System.console())
+					.map(Console::reader)
+					.map(BufferedReader::new),
+			Optional.ofNullable(System.console())
+					.map(Console::writer),
+			new SolverProgressLoggerFactory(),
+			solverProgressLogger ->
+					puzzleSolver -> puzzleSolver.setSolverProgressLogger(solverProgressLogger),
+			Thread::new);
+
+	/**
+	 * Dieses Feld muss die Fabrik für {@link PuzzleSolver} enthalten.
+	 */
+	@Nonnull
+	private final PuzzleSolverFactory puzzleSolverFactory;
+
+	/**
+	 * Dieses Feld muss den Thread für die Abarbeitung der Aufträge zum Wechsel des Ausgabemediums
+	 * im Hintergrund enthalten.
+	 */
+	@Nonnull
+	private final Thread backgroundThread;
+
+	/**
+	 * Dieses Feld muss das {@link Optional} für den {@link BufferedReader} für die Konsoleneingabe
+	 * enthalten.
+	 */
+	@Nonnull
+	private final Optional<BufferedReader> consoleReader;
+
+	/**
+	 * Dieses Feld muss das {@link Optional} für den {@link PrintWriter} für die Konsolenausgabe
+	 * enthalten.
+	 */
+	@Nonnull
+	private final Optional<PrintWriter> consoleWriter;
+
+	/**
+	 * Dieses Feld muss die Fabrik für verschiedene {@link SolverProgressLogger} enthalten.
+	 */
+	@Nonnull
+	private final SolverProgressLoggerFactory solverProgressLoggerFactory;
+
+	/**
+	 * Dieses Feld muss die Fabrikmethode für die Update-Aktion enthalten.
+	 */
+	@Nonnull
+	private final Function<SolverProgressLogger, Consumer<PuzzleSolver>> updateActionFactory;
+
+	/**
+	 * Die Singleton-Instance wird per {@link #getSingletonInstance()} bereitgestellt.
+	 *
+	 * @param puzzleSolverFactory die zu setzende {@link PuzzleSolverFactory}
+	 * @param consoleReader der zu setzende Reader für die Konsoleneingabe
+	 * @param consoleWriter der zu setzende Writer für die Konsolenausgabe
+	 * @param solverProgressLoggerFactory die zu setzende Fabrik für {@link SolverProgressLogger}
+	 * @param updateActionFactory die Fabrikmethode für Update-Aktionen
+	 * @param threadFactory die Fabrikmethode für {@link Thread}
+	 */
+	protected LogOutputManager(
+			@Nonnull final PuzzleSolverFactory puzzleSolverFactory,
+			@Nonnull final Optional<BufferedReader> consoleReader,
+			@Nonnull final Optional<PrintWriter> consoleWriter,
+			@Nonnull final SolverProgressLoggerFactory solverProgressLoggerFactory,
+			@Nonnull
+			final Function<SolverProgressLogger, Consumer<PuzzleSolver>> updateActionFactory,
+			@Nonnull final Function<Runnable, Thread> threadFactory) {
+
+		this.puzzleSolverFactory = puzzleSolverFactory;
+		this.consoleReader = consoleReader;
+		this.consoleWriter = consoleWriter;
+		this.solverProgressLoggerFactory = solverProgressLoggerFactory;
+		this.updateActionFactory = updateActionFactory;
+		this.backgroundThread = threadFactory.apply(this::showPromptRepeatedly);
+		this.backgroundThread.setDaemon(true);
+	}
+
+	/**
+	 * Diese Methode gibt die Singleton-Instance dieser Klasse zurück.
+	 *
+	 * @return die Instanz
+	 */
+	@Nonnull
+	public static LogOutputManager getSingletonInstance() {
+		return SINGLETON_INSTANCE;
+	}
+
+	/**
+	 * Diese Methode startet den Hintergrund-Thread, falls die für den Prompt benötigten Kanäle
+	 * verfügbar sind.
+	 */
+	public void init() {
+		if (this.consoleReader.isPresent() && this.consoleWriter.isPresent()) {
+			this.backgroundThread.start();
+		}
+	}
+
+	/**
+	 * Diese Methode führt die Abfrage der Einstellung wiederholt aus.
+	 */
+	protected void showPromptRepeatedly() {
+		while (true) {
+			try {
+				this.showPrompt();
+			} catch (EndOfInputException e) {
+				break;
+			}
+		}
+	}
+
+	/**
+	 * Diese Methode fragt die Einstellung für die Log-Ausgabe ab.
+	 */
+	private void showPrompt() {
+		this.consoleWriter
+				.ifPresent(this::writePrompt);
+		this.consoleReader
+				.map(this::readPrompt)
+				.ifPresentOrElse(
+						this::handleUserInput,
+						() -> {
+							throw new EndOfInputException();
+						});
+	}
+
+	/**
+	 * Diese Methode gibt die Abfragemeldung für die Einstellung der Log-Ausgabe in einen
+	 * {@link PrintWriter}.
+	 *
+	 * @param printWriter der zu verwendende Writer
+	 */
+	private void writePrompt(@Nonnull final PrintWriter printWriter) {
+		printWriter.println("Select log output [none | stdout]:");
+	}
+
+	/**
+	 * Diese Methode liest eine Zeile von der Konsole.
+	 *
+	 * @param reader der Reader von dem gelesen werden soll
+	 * @return die gelesene Zeile oder {@code null} bei End-of-Stream
+	 */
+	private String readPrompt(@Nonnull final BufferedReader reader) {
+		try {
+			return reader.readLine();
+		} catch (IOException e) {
+			throw new EndOfInputException();
+		}
+	}
+
+	/**
+	 * Diese Methode behandelt die Benutzereingabe.
+	 *
+	 * @param input die zu behandelnde Eingabe
+	 */
+	private void handleUserInput(@Nonnull final String input) {
+		if (StringUtils.equalsIgnoreCase(input, "none")) {
+			this.selectOutput(null);
+		} else if (StringUtils.equalsIgnoreCase(input, "stdout")) {
+			this.selectOutput(this.solverProgressLoggerFactory.createStdoutLogger());
+		} else {
+			this.consoleWriter
+					.ifPresent(this::writeInvalidInputMessage);
+		}
+	}
+
+	/**
+	 * Diese Methode schreibt die Meldung für ungültige Eingaben in einen {@link PrintWriter}.
+	 *
+	 * @param printWriter der zu verwendende Writer
+	 */
+	private void writeInvalidInputMessage(@Nonnull final PrintWriter printWriter) {
+		printWriter.println("Invalid log output!");
+	}
+
+	/**
+	 * Diese Methode legt die Ausgabe fest.
+	 *
+	 * @param solverProgressLogger der festzulegende Ausgabemechanismus oder {@code null} für keine
+	 *                             Ausgabe
+	 */
+	private void selectOutput(@CheckForNull final SolverProgressLogger solverProgressLogger) {
+		this.puzzleSolverFactory.updateAction(this.updateActionFactory.apply(solverProgressLogger));
+	}
+
+	/**
+	 * Diese Klasse stellt eine {@link RuntimeException} bereit, die anzeigt, dass die Eingabe
+	 * vollständig gelesen worden ist.
+	 */
+	protected static class EndOfInputException extends RuntimeException {
+
+		/**
+		 * Dieser Konstruktor nimmt die interne Initialisierung vor.
+		 */
+		protected EndOfInputException() {
+			super("End of input!");
+		}
+	}
+}
---
+++ src/test/java/software/sirsch/sa4e/puzzles/LogOutputManagerTest.java
@@ -0,0 +1,274 @@
+package software.sirsch.sa4e.puzzles;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Optional;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+import javax.annotation.Nonnull;
+
+import org.apache.commons.lang3.StringUtils;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InOrder;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.ArgumentMatchers.notNull;
+import static org.mockito.Mockito.inOrder;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
+import static org.mockito.Mockito.when;
+
+/**
+ * Diese Klasse stellt Tests für {@link LogOutputManager} bereit.
+ *
+ * @author sirsch
+ * @since 22.01.2023
+ */
+public class LogOutputManagerTest {
+
+	/**
+	 * Dieses Feld soll den Mock für {@link PuzzleSolverFactory} enthalten.
+	 */
+	private PuzzleSolverFactory puzzleSolverFactory;
+
+	/**
+	 * Dieses Feld soll den Mock für den {@link BufferedReader} zum Lesen der Konsoleneingabe
+	 * enthalten.
+	 */
+	private BufferedReader consoleReader;
+
+	/**
+	 * Dieses Feld soll den Mock für den {@link PrintWriter} zum Schreiben der Konsolenausgaben
+	 * enthalten.
+	 */
+	private PrintWriter consoleWriter;
+
+	/**
+	 * Dieses Feld soll den Mock für {@link SolverProgressLoggerFactory} enthalten.
+	 */
+	private SolverProgressLoggerFactory solverProgressLoggerFactory;
+
+	/**
+	 * Dieses Feld soll den Mock für die Fabrikmethode für die Update-Aktion enthalten.
+	 */
+	private Function<SolverProgressLogger, Consumer<PuzzleSolver>> updateActionFactory;
+
+	/**
+	 * Dieses Feld soll den Mock für den Hintergrund-Thread enthalten.
+	 */
+	private Thread backgroundThread;
+
+	/**
+	 * Dieses Feld soll den Mock für die Fabrikmethode für den Hintergrund-Thread enthalten.
+	 */
+	private Function<Runnable, Thread> threadFactory;
+
+	/**
+	 * Dieses Feld soll das zu testende Objekt enthalten.
+	 */
+	private LogOutputManager objectUnderTest;
+
+	/**
+	 * Diese Methode bereitet die Testumgebung für jeden Testfall vor.
+	 */
+	@BeforeEach
+	public void setUp() {
+		this.puzzleSolverFactory = mock(PuzzleSolverFactory.class);
+		this.consoleReader = mock(BufferedReader.class);
+		this.consoleWriter = mock(PrintWriter.class);
+		this.threadFactory = mock(Function.class);
+		this.solverProgressLoggerFactory = mock(SolverProgressLoggerFactory.class);
+		this.updateActionFactory = mock(Function.class);
+		this.backgroundThread = mock(Thread.class);
+		when(this.threadFactory.apply(notNull())).thenReturn(this.backgroundThread);
+
+		this.objectUnderTest = new LogOutputManager(
+				this.puzzleSolverFactory,
+				Optional.of(this.consoleReader),
+				Optional.of(this.consoleWriter),
+				this.solverProgressLoggerFactory,
+				this.updateActionFactory,
+				this.threadFactory);
+	}
+
+	/**
+	 * Diese Methode prüft {@link LogOutputManager#LogOutputManager(PuzzleSolverFactory, Optional,
+	 * Optional, SolverProgressLoggerFactory, Function, Function)}.
+	 *
+	 * @throws IOException wird in diesem Testfall nicht erwartet
+	 */
+	@Test
+	public void testConstructor() throws IOException {
+		ArgumentCaptor<Runnable> runnableArgumentCaptor = ArgumentCaptor.forClass(Runnable.class);
+
+		when(this.consoleReader.readLine()).thenReturn(null);
+
+		/* Das #objectUnderTest wird in Methode #setUp erzeugt. */
+
+		verify(this.threadFactory).apply(runnableArgumentCaptor.capture());
+		assertNotNull(runnableArgumentCaptor.getValue());
+		verifyNoInteractions(this.consoleReader);
+		verifyNoInteractions(this.consoleWriter);
+		runnableArgumentCaptor.getValue().run();
+		verify(this.consoleWriter).println(argThatStartsWith("Select log output"));
+	}
+
+	/**
+	 * Diese Methode prüft {@link LogOutputManager#getSingletonInstance()}.
+	 */
+	@Test
+	public void testGetSingletonInstance() {
+		assertSame(
+				LogOutputManager.getSingletonInstance(),
+				LogOutputManager.getSingletonInstance());
+	}
+
+	/**
+	 * Diese Methode prüft {@link LogOutputManager#init()}.
+	 */
+	@Test
+	public void testInit() {
+		this.objectUnderTest.init();
+
+		verify(this.backgroundThread).start();
+	}
+
+	/**
+	 * Diese Methode prüft {@link LogOutputManager#init()} wenn der Reader fehlt.
+	 */
+	@Test
+	public void testInitReaderNotPresent() {
+		this.objectUnderTest = new LogOutputManager(
+				this.puzzleSolverFactory,
+				Optional.of(this.consoleReader),
+				Optional.empty(),
+				this.solverProgressLoggerFactory,
+				this.updateActionFactory,
+				this.threadFactory);
+
+		this.objectUnderTest.init();
+
+		verify(this.backgroundThread, never()).start();
+	}
+
+	/**
+	 * Diese Methode prüft {@link LogOutputManager#init()} wenn der Writer fehlt.
+	 */
+	@Test
+	public void testInitWriterNotPresent() {
+		this.objectUnderTest = new LogOutputManager(
+				this.puzzleSolverFactory,
+				Optional.empty(),
+				Optional.of(this.consoleWriter),
+				this.solverProgressLoggerFactory,
+				this.updateActionFactory,
+				this.threadFactory);
+
+		this.objectUnderTest.init();
+
+		verify(this.backgroundThread, never()).start();
+	}
+
+	/**
+	 * Diese Methode prüft {@link LogOutputManager#showPromptRepeatedly()}.
+	 *
+	 * @throws IOException wird in diesem Testfall nicht erwartet
+	 */
+	@Test
+	public void testShowPromptRepeatedly() throws IOException {
+		InOrder orderVerifier = inOrder(
+				this.consoleReader,
+				this.consoleWriter,
+				this.puzzleSolverFactory);
+		SolverProgressLogger stdoutLogger = mock(SolverProgressLogger.class);
+		Consumer<PuzzleSolver> noneUpdateAction = mock(Consumer.class);
+		Consumer<PuzzleSolver> stdoutUpdateAction = mock(Consumer.class);
+
+		when(this.consoleReader.readLine()).thenReturn("none", "stdout", null);
+		when(this.solverProgressLoggerFactory.createStdoutLogger()).thenReturn(stdoutLogger);
+		when(this.updateActionFactory.apply(null)).thenReturn(noneUpdateAction);
+		when(this.updateActionFactory.apply(stdoutLogger)).thenReturn(stdoutUpdateAction);
+
+		this.objectUnderTest.showPromptRepeatedly();
+
+		orderVerifier.verify(this.consoleWriter).println(argThatStartsWith("Select log output"));
+		orderVerifier.verify(this.consoleReader).readLine();
+		orderVerifier.verify(this.puzzleSolverFactory).updateAction(noneUpdateAction);
+		orderVerifier.verify(this.consoleWriter).println(argThatStartsWith("Select log output"));
+		orderVerifier.verify(this.consoleReader).readLine();
+		orderVerifier.verify(this.puzzleSolverFactory).updateAction(stdoutUpdateAction);
+		orderVerifier.verify(this.consoleWriter).println(argThatStartsWith("Select log output"));
+		orderVerifier.verify(this.consoleReader).readLine();
+	}
+
+	/**
+	 * Diese Methode prüft {@link LogOutputManager#showPromptRepeatedly()} mit ungültiger Eingabe.
+	 *
+	 * @throws IOException wird in diesem Testfall nicht erwartet
+	 */
+	@Test
+	public void testShowPromptRepeatedlyInvalidInput() throws IOException {
+		InOrder orderVerifier = inOrder(
+				this.consoleReader,
+				this.consoleWriter,
+				this.puzzleSolverFactory);
+		SolverProgressLogger stdoutLogger = mock(SolverProgressLogger.class);
+		Consumer<PuzzleSolver> stdoutUpdateAction = mock(Consumer.class);
+
+		when(this.consoleReader.readLine()).thenReturn("invalid", "stdout", null);
+		when(this.solverProgressLoggerFactory.createStdoutLogger()).thenReturn(stdoutLogger);
+		when(this.updateActionFactory.apply(stdoutLogger)).thenReturn(stdoutUpdateAction);
+
+		this.objectUnderTest.showPromptRepeatedly();
+
+		orderVerifier.verify(this.consoleWriter).println(argThatStartsWith("Select log output"));
+		orderVerifier.verify(this.consoleReader).readLine();
+		orderVerifier.verify(this.consoleWriter).println("Invalid log output!");
+		orderVerifier.verify(this.consoleWriter).println(argThatStartsWith("Select log output"));
+		orderVerifier.verify(this.consoleReader).readLine();
+		orderVerifier.verify(this.puzzleSolverFactory).updateAction(stdoutUpdateAction);
+		orderVerifier.verify(this.consoleWriter).println(argThatStartsWith("Select log output"));
+		orderVerifier.verify(this.consoleReader).readLine();
+	}
+
+	/**
+	 * Diese Methode prüft {@link LogOutputManager#showPromptRepeatedly()} wenn eine
+	 * {@link IOException} auftritt.
+	 *
+	 * @throws IOException wird in diesem Testfall nicht erwartet
+	 */
+	@Test
+	public void testShowPromptRepeatedlyIOException() throws IOException {
+		InOrder orderVerifier = inOrder(
+				this.consoleReader,
+				this.consoleWriter,
+				this.puzzleSolverFactory);
+
+		when(this.consoleReader.readLine()).thenThrow(new IOException("testException"));
+
+		assertDoesNotThrow(() -> this.objectUnderTest.showPromptRepeatedly());
+
+		orderVerifier.verify(this.consoleWriter).println(argThatStartsWith("Select log output"));
+		orderVerifier.verify(this.consoleReader).readLine();
+	}
+
+	/**
+	 * Diese Methode erzeugt einen Argument-Matcher der einen String auf ein Präfix vergleicht.
+	 *
+	 * @param prefix das erwartete Präfix
+	 * @return der erzeugte Matcher
+	 */
+	@Nonnull
+	private static String argThatStartsWith(@Nonnull final String prefix) {
+		return argThat(string -> StringUtils.startsWith(string, prefix));
+	}
+}
